f06369c55c3b7f5f7f418426560820290d39a33c
Namee572 <myiddab@gmail.com>
2024-06-11 17:33:15 +0900

    studentApplication

--- studentAppApplication/studentAppApplication/repository/StudentRepository.cs
+++ studentAppApplication/studentAppApplication/repository/StudentRepository.cs
@@ -1,6 +1,8 @@
-﻿using studentAppApplication.model;
+﻿using studentAppApplication.conn;
+using studentAppApplication.model;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,13 @@ namespace studentAppApplication.repository
 {
     public class StudentRepository 
     {
+        private SqlConnection sqlConnection;
+        private SqlCommand sqlCommand;  
+
+        StudentRepository() { 
+            sqlConnection = DBConn.getConn();
+            sqlCommand.Connection = sqlConnection;
+        }
 
         public List<StudentModel> FindAll() 
         { 
@@ -17,7 +26,7 @@ namespace studentAppApplication.repository
             list.Add(
                 new StudentModel() {
                     Idx = 0,
-                    Name = "Name",
+                    Name = "성이름",
                     Math = 90,
                     Eng = 80,
                     Kor = 70
